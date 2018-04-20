5255be08db017830c67a688febd69c9cc3157048
zhoujingnan <2873556739@qq.com>
2018-04-20 21:19:43 +0800

    zjn

--- app/Http/Controllers/Home/HomeContactController.php
+++ app/Http/Controllers/Home/HomeContactController.php
@@ -2,12 +2,20 @@
 namespace App\Http\Controllers\Home;
 use App\Http\Controllers\Controller;
 use App\Back\CommonModel;
+use App\libs\phpmailer;
 class HomeContactController extends Controller{
 	public function index(){
 		$obj=new CommonModel();
 		//企业简介	
-		$net_data=json_decode(json_encode($obj->get('contact',"1=1")),true);		
+		$net_data=json_decode(json_encode($obj->get('contact',"1=1")),true);	
+// print_r($net_data);die;
 		return view("home.contact_list",['net_data'=>$net_data]);
+	}
+	public function redis(){
+		$arr=$_GET;
+		unset($arr['_token']);
+		$mail=new PHPMailer();
+		print_r($mail);die;
 	}
 }
 
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -137,7 +137,9 @@ Route::get("/homestory/index","Home\HomeStoryController@index");
 Route::get("/homemember/index","Home\HomeMemberController@index");
 //联系我们
 Route::get("/homecontact/index","Home\HomeContactController@index");
+Route::get("/homecontact/redis","Home\HomeContactController@redis");
 //服务
 Route::get("/homeserver/index","Home\HomeServerController@index");
 //关于我们
 Route::get("/homeabout/index","Home\HomeAboutController@index");
+
