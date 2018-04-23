30152dc023e1abb055f40d6c4ac84088a355e91f
768100410 <768100410@qq.com>
2018-04-23 09:50:03 +0800

    zqd

--- app/Http/Controllers/Home/HomeMemberController.php
+++ app/Http/Controllers/Home/HomeMemberController.php
@@ -9,7 +9,14 @@ class HomeMemberController extends Controller{
 		$net_data=json_decode(json_encode($obj->get('contact',"1=1")),true);
 		//查询会员
 		$member_data=json_decode(json_encode($obj->get('member',"1=1")),true);
-		return view("home.member_list",['net_data'=>$net_data,'member_data'=>$member_data]);
+		//左广告
+		$time = time();
+		$left_ad_data=json_decode(json_encode($obj->get('advertising',"ad_id%2=1 && start_time<$time && end_time>$time")),true);
+		//右广告
+		$right_ad_data=json_decode(json_encode($obj->get('advertising',"ad_id%2=0 && start_time<$time && end_time>$time")),true);
+		//友情链接
+		$link_data=json_decode(json_encode($obj->get('link',"1=1")),true);
+		return view("home.member_list",['net_data'=>$net_data,'member_data'=>$member_data,'left_ad_data'=>$left_ad_data,'right_ad_data'=>$right_ad_data,'link_data'=>$link_data]);
 	}
 }
 
--- app/Http/Controllers/Home/HomeStoryController.php
+++ app/Http/Controllers/Home/HomeStoryController.php
@@ -18,7 +18,14 @@ class HomeStoryController extends Controller{
 				}
 			}
 		}
-		return view("home.story_list",['net_data'=>$net_data,'story_data'=>$story_data]);
+		//左广告
+		$time = time();
+		$left_ad_data=json_decode(json_encode($obj->get('advertising',"ad_id%2=1 && start_time<$time && end_time>$time")),true);
+		//右广告
+		$right_ad_data=json_decode(json_encode($obj->get('advertising',"ad_id%2=0 && start_time<$time && end_time>$time")),true);
+		//友情链接
+		$link_data=json_decode(json_encode($obj->get('link',"1=1")),true);
+		return view("home.story_list",['net_data'=>$net_data,'story_data'=>$story_data,'left_ad_data'=>$left_ad_data,'right_ad_data'=>$right_ad_data,'link_data'=>$link_data]);
 	}
 }
 
